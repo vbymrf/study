c77243324b993313f052ec180df0c36f4eb93507
Evgeny <vbymrf@bk.ru>
2019-01-28 17:11:23 +0300

    Выполнил все задания. В последнем не сделал сокращение

---
+++ geekbrains.level1/Lesson3/Fractions.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Fractions
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fractions(int numerator, int denominator)
+        {
+             
+            this.numerator = numerator;
+            if (denominator > 0)
+                this.denominator = denominator;
+            else if (denominator < 0)
+            {
+                this.denominator = -1*denominator;
+                this.numerator *= -1;
+            }
+            else denominator = 1;
+
+        }
+        public Fractions()
+        {
+            this.numerator = 1;
+            this.denominator = 2;
+        }
+        public int Numerator
+        {
+            get { return this.numerator;}
+            set { this.numerator = value; }            
+        }
+        public int Denominator
+        {
+            get { return this.denominator; }
+            set{
+                if (value == 0) throw new ArgumentException("Знаменатель не может быть равен 0");
+                else if (value > 0)
+                    this.denominator = value;
+                else if (value < 0)
+                {
+                    this.denominator = -1 * value;
+                    this.numerator *= -1;
+                }
+            }
+        }
+        public int Nod(int y1, int y2)
+        {
+            int n, kon = Math.Max(y1, y2);
+            ArrayList sum=new ArrayList();
+            for (int i = 2; i <= kon; i++)
+            {
+                if (y1 % i == 0 && y2 % i == 0)  {
+                n = i;
+                kon =kon/i;
+                sum.Add(n);
+            }
+            }
+            n = 1;
+            foreach (int d in sum)
+            {
+                n = n * d;
+            }
+             return (y1*y2)/n;
+            
+        }
+        public Fractions Plus(Fractions tu)
+        {
+            Fractions one = new Fractions();
+            int x1, x2, y1, y2,nod;
+            x1 = this.numerator;
+            x2 = tu.numerator;
+            y1 = this.denominator;
+            y2 = tu.denominator;
+            nod=Nod(y1,y2);
+
+            one.numerator = (x1 * nod / y1) + (x2*nod / y2);
+            one.denominator = nod;
+            return one; 
+        }
+
+        
+        public Fractions Minus(Fractions tu)
+        {
+            Fractions one = new Fractions();
+            int x1, x2, y1, y2;
+            x1 = this.numerator;
+            x2 = tu.numerator;
+            y1 = this.denominator;
+            y2 = tu.denominator;
+            int nod = Nod(y1, y2);
+            one.numerator = (x1 * nod / y1) - (x2 * nod / y2);
+            one.denominator = nod;
+            return one;
+        }
+        public Fractions Multi(Fractions tu)
+        {
+            Fractions one = new Fractions();
+            int x1, x2, y1, y2;
+            x1 = this.numerator;
+            x2 = tu.numerator;
+            y1 = this.denominator;
+            y2 = tu.denominator;
+            int nod = Nod(y1, y2);
+            one.numerator = (x1 * nod / y1) * (x2 * nod / y2);
+            one.denominator = nod;
+            return one;
+        }
+        public Fractions Del(Fractions tu)
+        {
+            Fractions one = new Fractions();
+            int x1, x2, y1, y2;
+            x1 = this.numerator;
+            x2 = tu.numerator;
+            y1 = this.denominator;
+            y2 = tu.denominator;
+            int nod = Nod(y1, y2);
+            one.numerator = (x1 * nod / y1) / (x2 * nod / y2);
+            one.denominator = nod;
+            return one;
+        }
+        public double DoubleFractions()
+        {
+            return (double)this.numerator / (double)this.denominator;
+        }
+
+        public override string ToString()
+        {
+            return this.numerator+"/"+this.denominator;
+        }
+
+    
+    }
+}
--- geekbrains.level1/Lesson3/Program.cs
+++ geekbrains.level1/Lesson3/Program.cs
@@ -10,70 +10,87 @@ namespace Lesson3
     { 
         ​static​ ​void​ ​Main​(​string​[]​ args) 
         {
-//1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.                  
-//Продемонстрировать работу структуры. 
+////1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел.                  
+////Продемонстрировать работу структуры. 
              
-            Struct.Complex complex1; 
-            complex1​.​re ​=​ ​1; 
-            complex1​.​im ​=​ ​1;  
-            ​Struct.Complex complex2; 
-            complex2​.​re ​=​ ​2; 
-            complex2​.​im ​=​ ​2;
-            Console.WriteLine("Структура 1  = " + complex1 + "  ;  Структура 2  =  " + complex2);
-            Console.WriteLine("Структура 1 минус структура 2    = " + complex1.Minus(complex2));
+//            Struct.Complex complex1; 
+//            complex1​.​re ​=​ ​1; 
+//            complex1​.​im ​=​ ​1;  
+//             Struct.Complex complex2; 
+//            complex2​.​re ​=​ ​2; 
+//            complex2​.​im ​=​ ​2;
+//            Console.WriteLine("Структура 1  = " + complex1 + "  ;  Структура 2  =  " + complex2);
+//            Console.WriteLine("Структура 1 минус структура 2    = " + complex1.Minus(complex2));
 
-//  1.           б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить                      
-//              работу класса. 
-//               в) Добавить диалог с использованием switch демонстрирующий работу класса. 
-            Class.Complex complex3=new Class.Complex(2,4);            
-            Class.Complex complex4=new Class.Complex(5,2);
+////  1.           б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить                      
+////              работу класса. 
+////               в) Добавить диалог с использованием switch демонстрирующий работу класса. 
+//            Class.Complex complex3=new Class.Complex(2,4);            
+//            Class.Complex complex4=new Class.Complex(5,2);
             
-            Console.WriteLine("Структура 1  = " + complex3 + "  ;  Структура 2  =  " + complex4);
-            Console.WriteLine("Укажите операцию над комплекстными числами: + -  * ");
-            string st = Console.ReadLine();
-             switch (st) {
-                 case "+": Console.WriteLine("Структура 1 плюс структура 2    = " + complex3.Plus(complex4)); break;
-                 case "-": Console.WriteLine("Структура 1 минус структура 2    = " + complex3.Minus(complex4)); break;
-                 case "*": Console.WriteLine("Структура 1 умножить структура 2    = " + complex3.Multi(complex4)); break;
+//            Console.WriteLine("Структура 1  = " + complex3 + "  ;  Структура 2  =  " + complex4);
+//            Console.WriteLine("Укажите операцию над комплекстными числами: + -  * ");
+//            string st = Console.ReadLine();
+//             switch (st) {
+//                 case "+": Console.WriteLine("Структура 1 плюс структура 2    = " + complex3.Plus(complex4)); break;
+//                 case "-": Console.WriteLine("Структура 1 минус структура 2    = " + complex3.Minus(complex4)); break;
+//                 case "*": Console.WriteLine("Структура 1 умножить структура 2    = " + complex3.Multi(complex4)); break;
                  
-             }
-// 2. а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).                              
-//Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму                      
-//вывести на экран, используя tryParse. 
+//             }
+//// 2. а) С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).                              
+////Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму                      
+////вывести на экран, используя tryParse. 
 
-             string ch;
-             int vv,sum=0;
-             ArrayList Number=new ArrayList();
-             bool flag=true;
-             do
-             {
-                 Console.WriteLine("Введите положительное число , больше нуля:");
-                 Number.Add(Console.ReadLine());
+//             string ch;
+//             int vv,sum=0;
+//             ArrayList Number=new ArrayList();
+//             bool flag=true;
+//             do
+//             {
+//                 Console.WriteLine("Введите положительное число , больше нуля:");
+//                 Number.Add(Console.ReadLine());
                  
-                 if (!int.TryParse((string)Number[Number.Count-1], out vv) | vv < 0)
-                 {
-                     Console.WriteLine("Ввели не правильно");
-                     continue;
-                 }
-                 else if (!int.TryParse((string)Number[Number.Count-1], out vv) | vv == 0)
-                 {
-                     Console.WriteLine("ввод закончен \n Нечетные числа:");
-                     foreach (string str in Number ){
-                            int.TryParse(str, out vv);
+//                 if (!int.TryParse((string)Number[Number.Count-1], out vv) | vv < 0)
+//                 {
+//                     Console.WriteLine("Ввели не правильно");
+//                     continue;
+//                 }
+//                 else if (!int.TryParse((string)Number[Number.Count-1], out vv) | vv == 0)
+//                 {
+//                     Console.WriteLine("ввод закончен \n Нечетные числа:");
+//                     foreach (string str in Number ){
+//                            int.TryParse(str, out vv);
                          
-                         if (vv % 2 != 0)
-                         {
-                            Console.WriteLine(" "+ vv);
-                            sum += vv;
-                         }                         
-                     }
+//                         if (vv % 2 != 0)
+//                         {
+//                            Console.WriteLine(" "+ vv);
+//                            sum += vv;
+//                         }                         
+//                     }
                         
-                     Console.WriteLine("Сумма не четных чисел = "+ sum );
-                     flag = false;
-                 }
+//                     Console.WriteLine("Сумма не четных чисел = "+ sum );
+//                     flag = false;
+//                 }
 
-             } while (flag);
+//             } while (flag);
+
+
+//             3. *Описать класс дробей — рациональных чисел, являющихся отношением двух целых чисел.                      
+//Предусмотреть методы сложения, вычитания, умножения и деления дробей. Написать                  
+//программу, демонстрирующую все разработанные элементы класса. 
+//* Добавить свойства типа int для доступа к числителю и знаменателю; 
+//* Добавить свойство типа double только на чтение, чтобы получить десятичную дробь числа; 
+//** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение                    
+//ArgumentException("Знаменатель не может быть равен 0"); 
+//*** Добавить упрощение дробей. 
                       
+             // Создал класс Fraction Реализованы все пункты
+            
+             Fractions fr = new Fractions(1, 2);
+             Console.WriteLine(" Ведено : "+fr);
+             Console.WriteLine("Общий числитель : "+ fr.Nod(1, 2));
+             fr=fr.Plus(new Fractions(1, 2));
+             Console.WriteLine("(1/2)/ (1/2) Получили" + fr);
 
              Console.ReadKey();
         }
