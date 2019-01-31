700602e11482d0a33ee2c5e1d512a7340c13cfda
Evgeny <vbymrf@bk.ru>
2019-01-31 14:48:34 +0300

    Выполник к уроку 4 C#2 задания 1)1 -е задание в классе Program ввиде методов 2)2-е создал  клас StaticClass

---
+++ geekbrains.level1/Lesson4/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+    /*    1. Дан целочисленный массив из 20 элементов. Элементы массива могут принимать целые                      
+значения от –10 000 до 10 000 включительно. Заполнить случайными числами. Написать                        
+программу, позволяющую найти и вывести количество пар элементов массива, в которых только                        
+одно число делится на 3. В данной задаче под парой подразумевается два подряд идущих                            
+элемента массива. Например, для массива из пяти элементов: 6; 2; 9; –3; 6 ответ — 2.  
+     * */
+        public  int[] mas;
+        public  int Para()
+        {
+            int n=0;
+            mas=Mas(20,-10000,10000);
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (mas[i] % 3 == 0 || mas[i - 1] % 3 == 0) n++;
+            }                
+                return n;
+        }
+        public int[] Mas(int lengthMas, int min, int max)
+        {
+            int[] mas = new int[lengthMas];
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(min, max);
+            }
+                return mas;            
+        }
+        public static void PrintMas(int[] mas)
+        {
+            if (mas != null)
+            {
+                for (int i = 0; i < mas.Length; i++)
+                    Console.Write("[{0}]={1};\t", i, mas[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Задача 1
+            Console.WriteLine("Задача 1");
+            Program pr=new Program();            
+            Console.WriteLine(pr.Para());
+            PrintMas(pr.mas);
+            
+            // Задача 2
+            // Создан класс для нее StaticClass
+            Console.WriteLine("Задача 2");
+            int[] maC=StaticClass.FileArray("mas.txt");
+            try
+            {
+                PrintMas(maC);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+              
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ geekbrains.level1/Lesson4/StaticClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+   /* 2. Реализуйте задачу 1 в виде статического класса StaticClass; 
+а) Класс должен содержать статический метод, который принимает на вход массив и решает                          
+задачу 1; 
+б) *Добавьте статический метод для считывания массива из текстового файла. Метод должен                        
+возвращать массив целых чисел; 
+в)**Добавьте обработку ситуации отсутствия файла на диске.*/
+    static class StaticClass
+    {
+        static StreamReader sr;
+        public static int Para(int[] mas)
+        {
+            int n = 0;
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (mas[i] % 3 == 0 || mas[i - 1] % 3 == 0) n++;
+            }
+            return n;
+        }
+        public static int[] FileArray(string f_name)
+       {
+           int[] mas;
+         
+
+
+
+           try
+           {
+               sr = new StreamReader(f_name);
+               int LengtF=0;
+               while (sr.ReadLine() != null) LengtF++;
+               mas = new int[LengtF];
+               for (int i = 0; i < LengtF; i++)
+               {
+                   int n;
+                   
+                   n=int.Parse(sr.ReadLine());
+                   Console.WriteLine(n);
+                   //if (int.TryParse(sr.ReadLine(), out mas[i])) throw new Exception("Прочитали не верно. В массиве ["+i+"] записано 0 по умолчанию");
+                   //mas[i] = int.Parse(sr.ReadLine());
+               }
+
+               sr.Close();
+
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine(e.Message);
+               return null;
+           }
+           
+           return mas;
+       }
+    }
+}
