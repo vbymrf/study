3a61b9526478c023d108002b0f50fd5c6d404649
Evgeny <vbymrf@bk.ru>
2019-02-04 14:56:24 +0300

    Выполнил упражнение из урока 5 1) Поностью 2) В процессе

---
+++ geekbrains.level1/Lesson5/Messaga.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+
+
+    /// <summary>
+    ///  2. Разработать статический класс ​Message​, содержащий следующие статические методы для
+    /// обработки текста: 
+    /// </summary>
+
+    static class Messaga
+    {
+        /// <summary>
+        /// а) Вывести только те слова сообщения,  которые содержат не более n букв.
+        /// </summary>
+        /// <param name="mes"> сообщение</param>
+        /// <param name="n">количество букв</param>
+        /// <returns></returns>
+        static void ToStringNumber(string mes, int n)
+        {
+            string[] word = mes.Split(new char[] { ' ' });
+            foreach (string w in word)
+            {
+                if (w.Length < n) Console.WriteLine(w);
+            }
+        }
+        /// <summary>
+        /// б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+        /// </summary>
+        /// <param name="mes"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static string DeletWordSimbol(string mes, int n)
+        {
+            string[] word = mes.Split(new char[] { ' ' });
+            foreach (string w in word)
+            {
+                if (w.Length < n) Console.WriteLine(w);
+            }
+            return word[0];
+
+        }
+
+
+    }
+    //
+    //
+    //в) Найти самое длинное слово сообщения.
+    //г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.
+    //д) ***Создать метод, который производит частотный анализ текста.В качестве параметра в
+    //него передается массив слов и текст, в качестве результата метод возвращает сколько раз
+    //каждое из слов массива входит в этот текст.Здесь требуется использовать класс Dictionary.
+}
---
+++ geekbrains.level1/Lesson5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Lesson5
+{
+    class Program
+    {
+        /// <summary>
+        ///  1/ Создать программу, которая будет проверять корректность ввода логина.Корректным
+        ///логином будет строка от 2 до 10 символов, содержащая только буквы латинского алфавита
+        ///или цифры, при этом цифра не может быть первой: 
+        ///а) без использования регулярных выражений;
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns></returns>
+
+        static bool LogIn(string login)
+        {
+            if (login == null) return false;
+
+            if (login.Length < 2 || login.Length > 10) return false;
+            if (char.IsNumber(login[0])) return false;
+
+            for (int i = 0; i < login.Length; i++)
+            {
+                if (login[i] < (int)'0' || (login[i] > (int)'9' && login[0] < (int)'A') || (login[i] > (int)'Z' && login[i] < (int)'a')
+                || login[i] > (int)'z') return false;
+            }
+            return true;
+        }
+        /// <summary>
+        ///  1.) б) ** с использованием регулярных выражений.*/
+        /// </summary>
+        /// <param name="args"></param>
+        static bool LogIn2(string login)
+        {
+            Regex a = new Regex(@"^[a-zA-Z][a-zA-Z\d]{1,9}$");
+            if (a.IsMatch(login)) return true;
+            else return false;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 1 Проверка логина ");
+            // 1.a В метод  вставлен допустимый логин: 
+            Console.WriteLine("Проверка в цикле = " + LogIn("HGgfhj56"));
+            // 1.б В метод  вставлен допустимый логин: 
+            Console.WriteLine("Проверка регулярным выражением = " + LogIn2("h123456789"));
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
