8d7655bfe21274c1012241ad11b87d4b9a6a6756
Evgeny <vbymrf@bk.ru>
2019-02-07 17:40:55 +0300

    Выполнил задание 2

--- geekbrains.level1/Lesson6/Func.cs
+++ geekbrains.level1/Lesson6/Func.cs
@@ -9,16 +9,26 @@ using System.IO;
 типа double (double, double). Продемонстрировать работу на функции с функцией a*x^2 и                        
 функцией a*sin(x). 
 */
+/*
+ * 2. Модифицировать программу нахождения минимума функции так, чтобы можно было                  
+передавать функцию в виде делегата.  
+а) Сделать меню с различными функциями и представить пользователю выбор, для какой                        
+функции и на каком отрезке находить минимум. Использовать массив (или список) делегатов,                        
+в котором хранятся различные функции. 
+б) *Переделать функцию Load, чтобы она возвращала массив считанных значений. Пусть она                        
+возвращает минимум через параметр (с использованием модификатора out).  
+*/
+
 public delegate double FuncD(double x, double a);
 namespace Lesson6
 {
     class Func
     {
-        public static double F1(double x, double a) // Изменение 1
+        public static double F1(double x, double a) // Изменение для 2
         {
             return a * Math.Pow( x  , 2);
         }
-        public static double F2(double x, double a) // Изменение 2
+        public static double F2(double x, double a) // Изменение для 2
         {
             return a * Math.Sin(x);
         }
@@ -27,19 +37,47 @@ namespace Lesson6
             return x * x - 50 * x + 10;
         }
 
-        public static void SaveFunc(FuncD F,string fileName, double a, double b, double h, double ax)//Изменение 3
+        public static void SaveFunc(FuncD FD,string fileName, double a, double b, double h, double ax)//Изменение для 2 
         {
             FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             BinaryWriter bw = new BinaryWriter(fs);
             double x = a;
             while (x <= b)
             {
-                bw.Write(F(x,ax));
+                bw.Write(FD(x,ax));
                 x += h;// x=x+h;
             }
             bw.Close();
             fs.Close();
         }
+
+        public static void SaveFuncF1(string fileName, double a, double b, double h, double ax)//Изменение для 1
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F1(x, ax));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static void SaveFuncF2(string fileName, double a, double b, double h, double ax)//Изменение для 1
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F2(x, ax));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
         public static double Load(string fileName)
         {
             FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
--- geekbrains.level1/Lesson6/Program.cs
+++ geekbrains.level1/Lesson6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 using System.IO;
+using System.Collections;
 
 namespace Lesson6
 {
@@ -10,13 +11,34 @@ namespace Lesson6
         static void Main(string[] args)
         {
             // 1. Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
-                        Func.SaveFunc(new FuncD(Func.F1),"data.bin", -100, 100, 0.5,3);
+                        Func.SaveFuncF1("data.bin", -100, 100, 0.5,3);
             Console.WriteLine(Func.Load("data.bin"));
-            Func.SaveFunc(new FuncD(Func.F2), "data.bin", -100, 100, 0.5, 3);
+            Func.SaveFuncF2( "data.bin", -100, 100, 0.5, 3);
             Console.WriteLine(Func.Load("data.bin"));
-
-
-            Console.ReadKey();
+            // Через делегаты
+            Func.SaveFunc(new FuncD(Func.F2),"data.bin", -100, 100, 0.5, 3);
+            Console.WriteLine(Func.Load("data.bin"));
+            Func.SaveFunc(Func.F2,"data.bin", -100, 100, 0.5, 3);
+            Console.WriteLine(Func.Load("data.bin"));
+            /* 2. 2. Модифицировать программу нахождения минимума функции так, чтобы можно было                  
+            передавать функцию в виде делегата.
+            а) Сделать меню с различными функциями и представить пользователю выбор, для какой
+функции и на каком отрезке находить минимум.Использовать массив(или список) делегатов,                        
+в котором хранятся различные функции.
+б) *Переделать функцию Load, чтобы она возвращала массив считанных значений.Пусть она
+возвращает минимум через параметр(с использованием модификатора out).  */
+            ArrayList fun = new ArrayList();
+            fun.Add(new FuncD(Func.F));
+            fun.Add(new FuncD(Func.F1));
+            fun.Add(new FuncD(Func.F2));
+            Console.WriteLine("Укажите функцию 1=x * x-50*x+10 ; 2=a*x^2 ; 3=a*sin(x)");
+            int funcF =int.Parse(Console.ReadLine())-1;
+            Console.WriteLine("Задайте диапазоны от A до B шаг H и число Ax ввиде: A B H Ax ");
+            double[] per= Array.ConvertAll(Console.ReadLine().Split(' '), double.Parse);
+            Func.SaveFunc((FuncD)fun[funcF], "data.bin",per[0],per[1],per[2],per[3]);
+            Console.WriteLine(Func.Load("data.bin"));
+            double min;
+            double[] mas = Func.LoadWriteLine("data.bin", out min);
         }
     }
 }
